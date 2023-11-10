80df37747bc25ffab26cdc7ac918f59af73712eb
sheuerit <sheuerit@gmail.com>
2023-11-10 15:49:02 +0900

    dojoin

--- src/main/java/com/koreaIT/demo/vo/Article.java
+++ src/main/java/com/koreaIT/demo/vo/Article.java
@@ -7,6 +7,8 @@ import lombok.Data;
 @AllArgsConstructor
 public class Article {
 	private int id;
+	private String regDate;
+	private String updateDate;
 	private String title;
 	private String body;
 }

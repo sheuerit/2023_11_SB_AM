636f1894651f548305b2a5c5e145c1c3cd0b1405
sheuerit <sheuerit@gmail.com>
2023-11-10 13:59:39 +0900

    writeArticle, makeTestData

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -11,32 +11,49 @@ import com.koreaIT.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-
+	
 	private List<Article> articles;
 	private int lastArticleId;
-
+	
 	UsrArticleController() {
 		this.articles = new ArrayList<>();
 		this.lastArticleId = 0;
+		
+		makeTestData();
 	}
-
+	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			writeArticle(title, body);
+		}
+	}
+	
+	private Article writeArticle(String title, String body) {
+		this.lastArticleId++;
+		
+		Article article = new Article(lastArticleId, title, body);
+		
+		this.articles.add(article);
+		
+		return article;
+	}
+	
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
 	public Article doWrite(String title, String body) {
-
-		this.lastArticleId++;
-
-		Article article = new Article(this.lastArticleId, title, body);
-
-		articles.add(article);
-
+		
+		Article article = writeArticle(title, body);
+		
 		return article;
 	}
-
+	
 	@RequestMapping("/usr/article/showList")
 	@ResponseBody
 	public List<Article> showList() {
 		return this.articles;
 	}
-
+	
 }
